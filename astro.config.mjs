0f606ce40f65517f2bb899b9f153b476afacd3be
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2026-01-26 18:40:35 +0100

    fix: change the base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import netlify from '@astrojs/netlify';
+// import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  site: 'https://acsos.github.io/acsos-website/',
+  base: '/', // o il path specifico se deployi in una sottocartella
 });
